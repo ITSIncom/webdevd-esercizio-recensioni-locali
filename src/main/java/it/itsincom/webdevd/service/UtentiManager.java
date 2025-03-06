f92904c0d6be54760e4629c5a864cbc0ae7702c4
Giovanni Condello <giovanni.condello@coderit.it>
2025-03-06 11:35:55 +0100

    Implemntato flusso di registrazione in RegistrazioneResource
    Ho preparato anche l'UtentiManager per poter gestire gli utenti

---
+++ src/main/java/it/itsincom/webdevd/service/UtentiManager.java
@@ -0,0 +1,10 @@
+package it.itsincom.webdevd.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UtentiManager {
+    public boolean add(String username, String password) {
+        return true;
+    }
+}
