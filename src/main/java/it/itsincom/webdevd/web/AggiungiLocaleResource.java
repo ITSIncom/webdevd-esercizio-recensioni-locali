ead1a3a081a94bc94e91f246920472eed5b946d4
Diego Lo Cicero <diego.locicero@itsincom.it>
2025-03-06 15:12:50 +0100

    Creazione del file csv e implementazione del metodo SalvaLocale grazie alla sua classe resource

--- src/main/java/it/itsincom/webdevd/web/AggiungiLocaleResource.java
+++ src/main/java/it/itsincom/webdevd/web/AggiungiLocaleResource.java
@@ -2,8 +2,16 @@ package it.itsincom.webdevd.web;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @Path("/aggiungiLocale")
 public class AggiungiLocaleResource {
@@ -16,5 +24,32 @@ public class AggiungiLocaleResource {
     @GET
     public TemplateInstance mostraAggiungiLocale() {
         return aggiungiLocale.instance();
+    }
+
+    @POST
+    public TemplateInstance aggiungiLocalePost(
+            @FormParam("Nome") String nome,
+            @FormParam("Indirizzo") String indirizzo
+    )
+    {
+        String filePath = Paths.get("files", "locali.csv").toString();
+        File file = new File(filePath);
+        if (file.exists()) {
+            System.out.println("File trovato: " + file.getAbsolutePath());
+        } else {
+            System.out.println("File NON trovato!");
+            return null;
+        }
+        try (FileWriter writer = new FileWriter(filePath,true)) {
+            String data = "\n" + nome + "," + indirizzo;
+            writer.write(data);
+            System.out.println("Data: " + data + "appended succesfully");
+            return aggiungiLocale.instance();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
