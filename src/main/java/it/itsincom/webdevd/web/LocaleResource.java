ebe30cd4085b5aab405967de141c68af0186cea2
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-03-06 16:27:15 +0100

    Implementazione processLocale in LocalResource

--- src/main/java/it/itsincom/webdevd/web/LocaleResource.java
+++ src/main/java/it/itsincom/webdevd/web/LocaleResource.java
@@ -2,9 +2,14 @@ package it.itsincom.webdevd.web;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 @Path("/locale")
@@ -19,5 +24,17 @@ public class LocaleResource {
     @GET
     public TemplateInstance drawLocale() {
         return locale.instance();
+    }
+
+    @POST
+    public Response processLocale(@FormParam("nome") String nomeLocale
+                                ,@FormParam("indirizzo") String indirizzoLocale)  {
+        try(FileWriter fw = new FileWriter("locale.txt")) {
+            fw.write(nomeLocale + "," + indirizzoLocale);
+            return Response.ok().build();
+        }
+        catch (IOException e) {
+            return Response.serverError().build();
+        }
     }
 }
