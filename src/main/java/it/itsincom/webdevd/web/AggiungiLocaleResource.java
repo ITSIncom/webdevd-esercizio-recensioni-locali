aa025fcc959b23d7fa9726fe1a07b9466209a74c
Diego Lo Cicero <diego.locicero@itsincom.it>
2025-03-06 14:50:28 +0100

    preparazione aggiungi locale e inizializzazione della sua resource

--- src/main/java/it/itsincom/webdevd/web/AggiungiLocaleResource.java
+++ src/main/java/it/itsincom/webdevd/web/AggiungiLocaleResource.java
@@ -1,4 +1,20 @@
-package it.itsincom.webdevd.web.validation;
+package it.itsincom.webdevd.web;
 
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/aggiungiLocale")
 public class AggiungiLocaleResource {
+    private final Template aggiungiLocale;
+
+    public AggiungiLocaleResource(Template aggiungiLocale) {
+        this.aggiungiLocale = aggiungiLocale;
+    }
+
+    @GET
+    public TemplateInstance mostraAggiungiLocale() {
+        return aggiungiLocale.instance();
+    }
 }
