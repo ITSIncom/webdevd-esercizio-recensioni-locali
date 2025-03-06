e78aee1b39bad5a4547b0ec135bf3fe41d335e6f
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-03-06 17:38:55 +0100

    Aggiunta stringa associata a enum

--- src/main/java/it/itsincom/webdevd/web/validation/CredentialValidationErrors.java
+++ src/main/java/it/itsincom/webdevd/web/validation/CredentialValidationErrors.java
@@ -1,8 +1,20 @@
 package it.itsincom.webdevd.web.validation;
 
 public enum CredentialValidationErrors {
-    EMPTY_USERNAME,
-    EMPTY_PASSWORD,
-    DUPLICATE_USERNAME,
-    PASSSWORD_TOO_SHORT,
+    EMPTY_USERNAME("Lo username è vuoto"),
+    EMPTY_PASSWORD("La password è vuota"),
+    DUPLICATE_USERNAME("Lo username è già presente"),
+    PASSWORD_TOO_SHORT("La password è troppo corta");
+
+    private final String message;
+
+    CredentialValidationErrors(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
 }
+
