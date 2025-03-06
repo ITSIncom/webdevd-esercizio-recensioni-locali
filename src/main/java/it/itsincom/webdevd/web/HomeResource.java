ea2b72c91f3ed350834c6961986ba901426bcd96
TommasoBrivio <tommasobrivio150@gmail.com>
2025-03-06 15:40:08 +0100

    Implementata la logica della ricerca del locale

--- src/main/java/it/itsincom/webdevd/web/HomeResource.java
+++ src/main/java/it/itsincom/webdevd/web/HomeResource.java
@@ -2,20 +2,23 @@ package it.itsincom.webdevd.web;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
+import it.itsincom.webdevd.service.LocaliManager;
 import it.itsincom.webdevd.web.service.SessionManager;
-import jakarta.ws.rs.CookieParam;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+
+import java.util.Map;
 
 @Path("/")
 public class HomeResource {
 
     private final Template index;
     private final SessionManager sessionManager;
+    private final LocaliManager localiManager;
 
-    public HomeResource(Template index, SessionManager sessionManager) {
+    public HomeResource(Template index, SessionManager sessionManager, LocaliManager localiManager) {
         this.index = index;
         this.sessionManager = sessionManager;
+        this.localiManager = localiManager;
     }
 
     @GET
@@ -30,5 +33,17 @@ public class HomeResource {
             }
         }
         return index.data("loggedIn", isLoggedIn);
+    }
+
+    @POST
+    @Path("/ricerca")
+    public TemplateInstance ricercaLocali(@FormParam("search") String name){
+
+        if(name.length() >= 3){
+            Map<String, String> locali = localiManager.search(name);
+
+            return index.data("locali", locali);
+        }
+        return index.data("locali", "Devi inserire almeno 3 caratteri del nome del locale");
     }
 }
