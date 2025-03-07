2e65ece28e46af92f71fd5fe08f803e730e377e6
LucaMimmo <171295375+LucaMimmo05@users.noreply.github.com>
2025-03-07 19:39:17 +0100

    Aggiunta stampa username profilo

--- src/main/java/it/itsincom/webdevd/web/ProfileResource.java
+++ src/main/java/it/itsincom/webdevd/web/ProfileResource.java
@@ -2,24 +2,25 @@ package it.itsincom.webdevd.web;
 
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.QueryParam;
+import it.itsincom.webdevd.web.service.SessionManager;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
 
 @Path("/profile")
 public class ProfileResource {
 
     private final Template profile;
+    private final SessionManager sessionManager;
 
-    public ProfileResource(Template profile) {
+    public ProfileResource(Template profile, SessionManager sessionManager) {
         this.profile = profile;
+        this.sessionManager = sessionManager;
     }
 
     @GET
-    public TemplateInstance drawProfile() {
-        return profile.instance();
+    public TemplateInstance drawProfile(@CookieParam("Sessione") String idSession) {
+        String user = sessionManager.getUserFromSession(idSession);
+        return profile.data("username", user);
     }
 
     @POST
